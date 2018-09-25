d8b46b3d44e64697efd960bc1422d2a89feb1804
amicokaur <amica0811@yaho..com>
2018-09-24 21:27:46 -0700

    finished handlebars and file structure done

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,21 @@
+var db = require("./../models");
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        // Grab every document in the Articles collection
+        db.Article.find({})
+          .then(function(dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+         var data = {
+             articles: dbArticle
+         }
+        
+            res.render("index",data)
+          })
+          .catch(function(err) {
+            console.log("hitting error")
+            // If an error occurred, send it to the client
+            res.json(err);
+
+          });
+      });
+}
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require("express");
-var mongojs = require("mongojs");
 // Require request and cheerio. This makes the scraping possible
 var request = require("request");
 var cheerio = require("cheerio");
+var mongoose = require("mongoose");
+var axios = require("axios");
 
 // Initialize Express
 var app = express();
@@ -11,16 +12,21 @@ var app = express();
 var databaseUrl = "scraper";
 var collections = ["scrapedData"];
 
+// Require all models
+var db = require("./models");
+var exphbs = require("express-handlebars")
+app.engine("handlebars",exphbs({defaultLayout:"main"}));
+app.set("view engine", "handlebars");
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/week18Populater", { useNewUrlParser: true });
+
+
 // Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+
 
 // Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.send("Hello world");
-});
+
 
 // Retrieve data from the db
 app.get("/all", function(req, res) {
@@ -36,45 +42,9 @@ app.get("/all", function(req, res) {
     }
   });
 });
-
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-  // Make a request for the news section of `ycombinator`
-  request("https://news.ycombinator.com/", function(error, response, html) {
-    // Load the html body from request into cheerio
-    var $ = cheerio.load(html);
-    // For each element with a "title" class
-    $(".title").each(function(i, element) {
-      // Save the text and href of each link enclosed in the current element
-      var title = $(element).children("a").text();
-      var link = $(element).children("a").attr("href");
-
-      // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
-      }
-    });
-  });
-
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
-
-
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+   
 // Listen on port 3000
 app.listen(3000, function() {
   console.log("App running on port 3000!");
