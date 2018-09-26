2c41ccf98927c1c8a139cd1223f255c267d076f8
amicokaur <amica0811@yaho..com>
2018-09-26 14:32:38 -0700

    update MVP

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,42 @@
+
 $(function(){
     $(".articles").on("click",function(event){
         var id = $(this).data("id")
-        $(".card-header").data("id", id);
-        
-        
+   
+       $("#submit").attr("data-id", id);
+     displayComment(id);
     })
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+        var id = $("#submit").data("id"); 
+        var title = $("#commentTitle").val();
+        var body = $("#commentBody").val();
+        
+        var note = {
+            title: title,
+            body:  body
+        }
+      
+        $.ajax({
+            url:"/articles/"+id,
+            method:"POST",
+            data:note
+        }).then(function(data){
+            console.log("post done")
+            displayComment(id);
+        })
+    })
+    function displayComment (id){
+        $.ajax({
+            url:"/articles/" +id,
+            method:"GET"
+        }).then(function(data){
+        
+            $("#postTitle").html(data.note.title);
+            $("#postBody").text(data.note.body);
+        })
+    }
 })
+
+
+
